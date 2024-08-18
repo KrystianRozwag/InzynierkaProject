4bbcadad60171fc96ea45d33e6c9f3dd150ab82c
krystian.rozwag <krystek_1@onet.eu>
2024-08-18 18:28:19 +0200

    moved project to unreal engine 5.4

--- Source/FPSAIProj.Target.cs
+++ Source/FPSAIProj.Target.cs
@@ -8,8 +8,8 @@ public class FPSAIProjTarget : TargetRules
 	public FPSAIProjTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("FPSAIProj");
 	}
 }
